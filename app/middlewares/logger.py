432790b395d28089f53644242b5f54a50f344461
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-10-26 21:55:37 +0800

    Update the list of packages

--- app/middlewares/logger.py
+++ app/middlewares/logger.py
@@ -5,11 +5,26 @@ standard_logger = StandardLoggerService()
 
 def standard_logger_middleware(handler):
     def wrapper(event, context):
-        standard_logger.info("Incoming event", event=event, context=context)
+
+        if not hasattr(context, 'cold_start'):
+            context.cold_start = True
+        else:
+            context.cold_start = False
+
+        buffer = {
+            "name": context.function_name,
+            "version": context.function_version,
+            "arn": context.invoked_function_arn,
+            "memory_size": context.memory_limit_in_mb,
+            "aws_request_id": context.aws_request_id,
+            "cold_start": context.cold_start,
+        }
+
+        standard_logger.info("Input Data", input_data=event, lambda_function=buffer)
 
         response = handler(event, context)
 
-        standard_logger.info("Response", response=response)
+        standard_logger.info("Output Data", output_data=response)
 
         return response
 
