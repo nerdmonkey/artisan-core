7cd22da7aa3d687fe4d6ed47b50ce445241f6bde
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-12-27 11:36:07 +0800

    Update database URL construction and enhance test fixtures with docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,12 +9,30 @@ from app.helpers.database import db
 from app.models.base import Base
 from app.models.user import User
 
-load_dotenv(dotenv_path=".env_testing")
+load_dotenv(dotenv_path=".env")
 
 get_db = db()
 
 
 def construct_database_url():
+    """
+    Constructs a database URL based on environment variables.
+
+    The function supports SQLite, PostgreSQL, and MySQL databases. It reads the
+    following environment variables to construct the URL:
+
+    - DB_TYPE: The type of the database (default is "sqlite").
+    - DB_HOST: The database host (default is "localhost").
+    - DB_PORT: The database port (default is "5432").
+    - DB_USERNAME: The database username (default is "user").
+    - DB_PASSWORD: The database password (default is "password").
+
+    Returns:
+        str: The constructed database URL.
+
+    Raises:
+        ValueError: If the specified database type is unsupported.
+    """
     db_type = os.environ.get("DB_TYPE", "sqlite")
     db_name = "spartan.db"
     if db_type == "sqlite":
@@ -33,11 +51,19 @@ def construct_database_url():
         else:
             raise ValueError(f"Unsupported database type: {db_type}")
 
-    raise ValueError(f"Unsupported database type: {db_type}")
-
 
 @pytest.fixture(scope="module")
 def test_db_session():
+    """
+    Fixture to set up and tear down a test database session.
+
+    This fixture creates an engine and a session for a test database,
+    yielding the session to be used in tests. After the tests are done,
+    it closes the session and drops all tables in the test database.
+
+    Yields:
+        db (Session): SQLAlchemy session object for the test database.
+    """
     TEST_DATABASE_URL = construct_database_url()
 
     engine = create_engine(TEST_DATABASE_URL)
@@ -54,6 +80,19 @@ def test_db_session():
 
 @pytest.fixture(scope="module")
 def test_data(test_db_session):
+    """
+    Fixture to create and yield test user data.
+
+    This fixture creates five test users, adds them to the test database session,
+    and commits the session. After yielding the users, it cleans up by deleting
+    the users from the test database session and committing the session again.
+
+    Args:
+        test_db_session: The database session used for testing.
+
+    Yields:
+        list: A list of created User objects.
+    """
     users = [
         User(
             username=f"testuser{i}",
